5d8daf72a4481ca40c2cb11af6290a05f811d8cc
YaroslavnaYermolenko <162818983+YaroslavnaYermolenko@users.noreply.github.com>
2024-08-20 21:09:16 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -2,6 +2,7 @@ function slugify(title) {
   const array = title.toLowerCase().split(" ");
   return array.join("-");
 }
+
 console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
